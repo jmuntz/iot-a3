903b67929bbfaf0213d45152d267b72dfee31afd
Josh <1728645+jmuntz@users.noreply.github.com>
2020-06-07 15:16:49 +0100

    Working sample to save data to database.

---
+++ python/working_sample.py
@@ -0,0 +1,71 @@
+import serial
+import socket
+import sys
+import json
+import time
+import requests
+
+import urllib.request
+import random
+
+randnum = random.randint(10,26)
+
+server = 'https://iot.porky.dev/ass3/app'
+
+# posty = requests.post(host + '/functions.php', params=test_data, headers = {"User-Agent": "Firefox/12.0"});
+
+
+
+def is_json(data):
+    try:
+        json_object = json.loads(data)
+    except ValueError as e:
+        return False
+    return True
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.flush()
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+
+    while True:
+        if ser.in_waiting > 0:
+            data = ser.readline().decode('utf-8').rstrip(' ')
+            print("SERIAL DATA: " + "["+data+"]")
+            json_data = json.loads(data)
+            
+            temperature = json_data["temp"]
+            humidity = json_data["humidity"]
+            
+            
+            #if is_json(data):
+           
+            temp_req = urllib.request.Request(server + '/api/save/temperature')
+            temp_req.add_header('Content-Type', 'application/json; charset=utf-8')
+            jsondata = json.dumps(json_data)
+            jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+            temp_req.add_header('Content-Length', len(jsondataasbytes))
+            #print (jsondataasbytes)
+            temp_response = urllib.request.urlopen(temp_req, jsondataasbytes)
+            
+            txt_response = temp_response.read()
+            
+            my_json = json.loads(txt_response)   
+            print(my_json)
+            
+            
+            
+            hum_req = urllib.request.Request(server + '/api/save/humidity')
+            hum_req.add_header('Content-Type', 'application/json; charset=utf-8')
+
+            hum_req.add_header('Content-Length', len(jsondataasbytes))
+            #print (jsondataasbytes)
+            hum_response = urllib.request.urlopen(hum_req, jsondataasbytes)
+            
+            txt_response = hum_response.read()
+            
+            my_json = json.loads(txt_response)   
+            print(my_json)
+        #ser.flush()
+
