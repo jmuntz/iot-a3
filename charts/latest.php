538ea82d13c0f3f89ffb0ea7e24dae2f6f409be7
Josh <1728645+jmuntz@users.noreply.github.com>
2020-06-08 03:56:08 +0100

    Big cleanup. Added data toggling.
    Removed redundant functions. Added data toggling between temperature and humidity values.

--- charts/latest.php
+++ charts/latest.php
@@ -18,100 +18,100 @@
 			<button type="submit" name="button"> Login</button>
 		</form>
 		<?php else : ?>
-		<ul class="menu">
+		<ul class="nav menu">
 			<li><a class="btn" href=<?php echo '"'.$root_URL .'"'; ?>>Home</a></li>
 			<li><a class="btn logout" href="logout.php">Logout</a></li>
 		</ul>
-		<h3>You're logged in!</h3>
-		
-		
-		<div class="charts">
-			<div class="chart">
-				<h2> Latest data</h2>
-				<canvas id="chart" style="max-width: 800px; max-height: 600px;"></canvas>
-			</div>
+		<ul class="nav chart">
+			<li><strong>Charts</strong></li>
+			<li><a href=<?php echo '"'.$root_URL .'/charts/latest.php"'; ?>>Latest</a></li>
+			<li><a href=<?php echo '"'.$root_URL .'/charts/mean.php"'; ?>>Mean</a></li>
+			<li><a href=<?php echo '"'.$root_URL .'/charts/median.php"'; ?>>Median</a></li>
+		</ul>
+		<ul class="nav todo">
+			<li><strong>todo's</strong>
+				<li>Build out median chart</li>
+				<li>Build out mean chart</li>
+				<li style="text-decoration: line-through;">Add humidity to <em>latest</em> chart chart</li>
+				<li>Add humidity to <em>latest</em> chart chart</li>
+				<li>Add feature to allow actuator to act based on temperature</li>
+				<li> - needs to be editable via website</li>
+				<li> Report</li>
+			</ul>
+			<h3>You're logged in!</h3>
 			
-		</div>
-		<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-		<script>
-			let clients = [];
-			let tempJSON;
-			let humJSON;
-			let temp_values = [];
-			let hum_values = [];
-			function getTotalHosts() {
-				fetch('//iot.porky.dev/ass3/app/api/get/hosts')
-				.then(response => response.json())
-				.then(function(data) {
-					clients = data;
-				})
-			}
-			function generateDatasets() {
-					let temp_dataset 	= [];
-					let hum_dataset 	= [];
-				let boss_data = {};
-				var promise = new Promise(function(resolve, reject) {
-					getTotalHosts();
-					setTimeout(function() {
-						clients.forEach((client, i) => {
-							dataset = {};
-							arr = [];
-							tempJSON.forEach((data, i) => {
-								if (data.host == client)
-									arr.push(data.data);
-							});
-							dataset.host = client;
-							dataset.data = arr;
-							temp_dataset.push(dataset);
-							dataset = {};
-							arr = [];
-							humJSON.forEach((data, i) => {
-								if (data.host == client)
-								arr.push(data.data);
-							});
-							dataset.host = client;
-							dataset.data = arr;
-							hum_dataset.push(dataset);
-						});
-						boss_data.temp = temp_dataset;
-						boss_data.hum = hum_dataset;
-						resolve(boss_data);
-					}, 1000); // set timeout to give fetches time to finish
+			
+			<div class="charts">
+				<div class="chart">
+					<h2> Latest data</h2>
+					<p> Gets the last 25 results for each client device.</p>
+					<canvas id="chart" style="max-width: 800px; max-height: 600px;"></canvas>
+				</div>
+				
+				<button onclick=rechartTemperature();>Temperature</button>
+				<button onclick=rechartHumidity();>Humidity</button>
+			</div>
+			<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+			<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+			<script>
+				let clients = [];
+				function getTotalHosts() {
+					fetch('//iot.porky.dev/ass3/app/api/get/hosts')
+					.then(response => response.json())
+					.then(function(data) {
+						clients = data;
+					})
+				}
+				
+				function getAll() {
+					return fetch('https://iot.porky.dev/ass3/app/api/get')
+					.then(response => response.json())
+				}
+				getAll().then(function(data) {
+					console.log(data);
+				}).catch(function(error) {
+					console.log("getAll request failed.");
 				});
-				return promise;
-			};
-			function getAll() {
-				return fetch('https://iot.porky.dev/ass3/app/api/get')
-				.then(response => response.json())
-			}
-			getAll().then(function(data) {
-				console.log(data);
-			}).catch(function(error) {
-				console.log("getAll request failed.");
-			});
-			let labels = [];
-			for (i = 0; i < 25; i++) { labels[i] = i; }
-			var ctx = document.getElementById('chart').getContext('2d');
-			var chart = new Chart(ctx, {
-				type: 'line',
-				data: {
-					labels: labels,
-				},
-				options: {}
-			});
-			updateChart();
-			function updateChart() {
-					temp = {};
+				let labels = [];
+				for (i = 0; i < 25; i++) { labels[i] = i; }
+				var ctx = document.getElementById('chart').getContext('2d');
+				var chart = new Chart(ctx, {
+					type: 'line',
+					data: {
+						labels: labels,
+					},
+					options: {}
+				});
+				updateChart();
+				function updateChart() {
+						getAll().then(function(data) {
+							dataset = [{}];
+							
+							for (index in data.temperature) {
+								dataset[index] = {
+									// label: "Client " + index,//data.temperature[index].host,
+									label: data.temperature[index].client_addr,//data.temperature[index].host,
+									labels: labels,
+									borderColor: "red",
+									backgroundColor: 'transparent',
+									data: data.temperature[index].data
+								}
+							}
+							chart.config.data.datasets = dataset;
+							chart.update();
+						}).catch(function(error) {
+							console.log("getAll chart request failed.");
+						});
+				};
+				function rechartTemperature() {
 					getAll().then(function(data) {
-						console.log(data);
-						console.log(data.temperature);
-						console.log(data.humidity);
 						dataset = [{}];
-						for(index in data.temperature) {
+						
+						for (index in data.temperature) {
 							dataset[index] = {
-								label: "Client " + index,//data.temperature[index].host,
+								// label: "Client " + index,//data.temperature[index].host,
+								label: data.temperature[index].client_addr,//data.temperature[index].host,
 								labels: labels,
 								borderColor: "red",
 								backgroundColor: 'transparent',
@@ -123,8 +123,28 @@
 					}).catch(function(error) {
 						console.log("getAll chart request failed.");
 					});
-			};
-		</script>
-		<?php endif; ?>
-	</body>
-</html>
+				};
+				function rechartHumidity() {
+					getAll().then(function(data) {
+						dataset = [{}];
+						
+						for (index in data.humidity) {
+							dataset[index] = {
+								// label: "Client " + index,//data.humidity[index].host,
+								label: data.humidity[index].client_addr,//data.temperature[index].host,
+								labels: labels,
+								borderColor: "purple",
+								backgroundColor: 'transparent',
+								data: data.humidity[index].data
+							}
+						}
+						chart.config.data.datasets = dataset;
+						chart.update();
+					}).catch(function(error) {
+						console.log("getAll chart request failed.");
+					});
+				};
+			</script>
+			<?php endif; ?>
+		</body>
+	</html>
