62c8083d7584d02c2a56211c616d55c09c500d04
Josh Muntz <gandlaf@192-168-1-112.tpgi.com.au>
2020-06-13 21:26:13 +1000

    Added UpdateController.php.

--- api/index.php
+++ api/index.php
@@ -3,15 +3,15 @@ session_start();
 
 require __DIR__ . '/../source/RestServer/RestServer.php';
 // require 'config.php';
-require 'Controller.php';
 require 'SaveController.php';
 require 'GetController.php';
+require 'UpdateController.php';
 
 $server = new \RestServer\RestServer('debug');
 $server->useCors = true;
 $server->allowedOrigin = '*';
 
-$server->addClass('Controller');
 $server->addClass('SaveController', '/save');
 $server->addClass('GetController', '/get');
+$server->addClass('UpdateController', '/update');
 $server->handle();
